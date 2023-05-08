0a72d71ea46fe7ae0843b1e105095947964dcea6
Hugo Ramon <ramone.techie@gmail.com>
2023-05-08 13:26:56 -0300

    Added custom fonts

--- Frontend/src/components/Orders/index.tsx
+++ Frontend/src/components/Orders/index.tsx
@@ -2,22 +2,51 @@ import { Order } from '../../types/Order';
 import OrdersBoard from '../OrdersBoard';
 import * as Styled from './styles';
 
+// const orders: Order[] = [
+//   {
+//     '_id': '64570e202f1e3b86577c2304',
+//     'table': '3',
+//     'status': 'WAITING',
+//     'products': [
+//       {
+//         'product': {
+//           'name': 'Coca cola',
+//           'imagePath': '1683399063393-coca-cola.png',
+//           'price': 6,
+//         },
+//         'quantity': 1,
+//         '_id': '64570e202f1e3b86577c2305'
+//       }
+//     ]
+//   }
+// ];
+
+// From Mateus Silva
 const orders: Order[] = [
   {
-    '_id': '64570e202f1e3b86577c2304',
-    'table': '3',
-    'status': 'WAITING',
-    'products': [
+    _id: '6372e48cbcd195b0d3d0f7f3',
+    table: '123',
+    status: 'WAITING',
+    products: [
       {
-        'product': {
-          'name': 'Coca cola',
-          'imagePath': '1683399063393-coca-cola.png',
-          'price': 6,
+        product: {
+          name: 'Pizza quatro queijos',
+          imagePath: '1668472896991-quatro-queijos.png',
+          price: 40,
         },
-        'quantity': 1,
-        '_id': '64570e202f1e3b86577c2305'
+        quantity: 3,
+        _id: '6372e48cbcd195b0d3d0f7f4'
+      },
+      {
+        product: {
+          name: 'Coca cola',
+          imagePath: '1668473462705-coca-cola.png',
+          price: 7,
+        },
+        quantity: 2,
+        _id: '6372e48cbcd195b0d3d0f7f5'
       }
-    ]
+    ],
   }
 ];
 
--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,10 +1,31 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Manrope-300': require('./src/assets/fonts/Manrope-Light-300.otf'),
+    'Manrope-500': require('./src/assets/fonts/Manrope-Medium-500.otf'),
+    'Manrope-600': require('./src/assets/fonts/Manrope-Semibold-600.otf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Ramon Pereira - React Native Developer - Next.js</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'Manrope-300' }}>Ramon Pereira - React Native Developer</Text>
       <StatusBar style="auto" />
     </View>
   );
