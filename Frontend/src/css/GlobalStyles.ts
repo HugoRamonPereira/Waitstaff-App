cb41e8426b103b43f47846d9e6eb2bf3b00152a0
Hugo Ramon <ramone.techie@gmail.com>
2023-10-10 20:26:10 -0300

    Removed font face

--- Frontend/src/css/GlobalStyles.ts
+++ Frontend/src/css/GlobalStyles.ts
@@ -1,16 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
-import ManropeLightTtf from '../assets/fonts/Manrope-Light.ttf';
-import ManropeLightWoff from '../assets/fonts/Manrope-Light.woff';
-import ManropeLightWoff2 from '../assets/fonts/Manrope-Light.woff2';
-import ManropeMediumTtf from '../assets/fonts/Manrope-Medium.ttf';
-import ManropeMediumWoff from '../assets/fonts/Manrope-Medium.woff';
-import ManropeMediumWoff2 from '../assets/fonts/Manrope-Medium.woff2';
-import ManropeSemiboldTtf from '../assets/fonts/Manrope-Semibold.ttf';
-import ManropeSemiboldWoff from '../assets/fonts/Manrope-Semibold.woff';
-import ManropeSemiboldWoff2 from '../assets/fonts/Manrope-Semibold.woff2';
-
 export const GlobalStyles = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@300;500;600&display=swap');
 
 * {
     margin: 0;
@@ -28,35 +20,5 @@ export const GlobalStyles = createGlobalStyle`
     cursor: pointer;
     font-size: 1rem;
     color: #333;
-  }
-
-  @font-face {
-    font-family: 'Manrope';
-    font-weight: 300;
-    font-display: 'swap';
-    font-style: 'normal';
-    src: url('${ManropeLightTtf}') format('truetype'),
-         url('${ManropeLightWoff}') format('woff'),
-         url('${ManropeLightWoff2}') format('woff2');
-  }
-
-  @font-face {
-    font-family: 'Manrope';
-    font-weight: 500;
-    font-display: 'swap';
-    font-style: 'normal';
-    src: url('${ManropeMediumTtf}') format('truetype'),
-         url('${ManropeMediumWoff}') format('woff'),
-         url('${ManropeMediumWoff2}') format('woff2');
-  }
-
-  @font-face {
-    font-family: 'Manrope';
-    font-weight: 600;
-    font-display: 'swap';
-    font-style: 'normal';
-    src: url('${ManropeSemiboldTtf}') format('truetype'),
-         url('${ManropeSemiboldWoff}') format('woff'),
-         url('${ManropeSemiboldWoff2}') format('woff2');
   }
 `;
